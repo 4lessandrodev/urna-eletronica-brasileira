852a7b5f9c1d48a58ba767e12a4c5f08c881d5b1
4lessandrodev <alessandroadm@live.com>
2022-04-07 03:40:42 -0300

    feat(storybook): added args to stories

--- src/components/Title/stories.tsx
+++ src/components/Title/stories.tsx
@@ -14,11 +14,12 @@ export default {
 } as ComponentMeta<typeof Title>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<any> = (args) => <Title {...args} value="hello there" />;
+const Template: ComponentStory<any> = (args) => <Title {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
     primary: true,
     label: 'Title',
+    value: 'hello there',
 };
