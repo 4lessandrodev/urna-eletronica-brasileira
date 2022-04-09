135dad210af24eddfdbb279c6e198d28fc3eaae5
4lessandrodev <alessandroadm@live.com>
2022-04-09 01:21:25 -0300

    style(theme): added new attributes to app theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,9 +3,28 @@ import { ThemeType } from '@types';
 export default {
     font: {
         family: {
-            default: 'Fira Sans Condensed',
+            default: "'Fira Sans Condensed', sans-serif",
             secondary: 'Roboto',
-            third: 'sans-serif',
+            third: "'Roboto', sans-serif",
+        },
+        weights: {
+            extraLight: '200',
+            light: '300',
+            regular: '400',
+            medium: '500',
+            semiBold: '600',
+            bold: '700',
+            extraBold: '800',
+        },
+        sizes: {
+            xsmall: '0.5rem',
+            small: '0.7rem',
+            xnormal: '0.9rem',
+            normal: '1rem',
+            title: '1.6rem',
+            large: '2rem',
+            xlarge: '4rem',
+            xxlarge: '6rem',
         },
     },
     colors: {
@@ -26,5 +45,24 @@ export default {
         black81: '#252930',
         black87: '#161B20',
         black89: '#0C171B',
+    },
+    borderRadius: {
+        xsmall: '2px',
+        small: '3px',
+        xmedium: '5px',
+        medium: '7px',
+        xlarge: '34px',
+        large: '35px',
+    },
+    spacings: {
+        xsmall: '0.2rem',
+        xxsmall: '0.4rem',
+        small: '0.7rem',
+        xmedium: '1rem',
+        xxmedium: '1.2rem',
+        medium: '1.5rem',
+        xlarge: '2rem',
+        xxlarge: '2.rem',
+        large: '3rem',
     },
 } as Partial<ThemeType>;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,106 @@
 export interface FontFamilyType {
+  /** Fira Sans Condensed */
   default: string;
+   /** Roboto */
   secondary: string;
+  /** sans-serif */
   third: string
 }
 
+export interface Weights {
+  /** 200 */
+  extraLight: string;
+  /** 300 */
+  light: string;
+  /** 400 */
+  regular: string;
+  /** 500 */
+  medium: string;
+  /** 600 */
+  semiBold: string;
+  /** 700 */
+  bold: string;
+  /** 800 */
+  extraBold: string;
+}
+
+export interface FontSizes {
+  xsmall: string;
+  small: string;
+  xnormal: string;
+  normal: string;
+  title: string;
+  large: string;
+  xlarge: string;
+  xxlarge: string;
+}
 export interface FontTypes {
-  family: FontFamilyType
+  family: FontFamilyType;
+  weights: Weights;
+  sizes: FontSizes;
 }
 
 export interface SpacingTypes {
-  large: string;
+  xsmall: string;
+  xxsmall: string;
+  small: string;
+  xmedium: string;
+  xxmedium: string;
   medium: string;
+  xlarge: string;
+  xxlarge: string;
+  large: string;
+}
+
+export interface BordersRadius {
+  /** 2px */
+  xsmall: string;
+  /** 3px */
+  small: string;
+  /** 5px */
+  xmedium: string;
+  /** 7px */
+  medium: string;
+  /** 34px */
+  xlarge: string;
+  /** 35px */
+  large: string;
 }
 
 export interface ColorsType {
+  /** C3D1EE */
   primary: string;
+  /** 252930 */
   secondary: string;
+  /** 159950 */
   green: string;
+  /** 0E612D */
   darkGreen: string;
+  /** D76237 */
   orange: string;
+  /** AA4D2B */
   darkOrange: string;
+  /** CCCCCC */
   gray: string;
+  /** B4B7C0 */
   darkGray: string;
+  /** EBF5FE */
   lightBlue251: string,
+  /** ECF3FD */
   lightBlue250: string,
+  /** D2E0FD */
   lightBlue242: string,
+  /** CFD9F2 */
   lightBlue241: string,
+  /** C3D1EE */
   lightBlue240: string,
+  /** FFFFFF */
   white: string;
+  /** 252930 */
   black81: string;
+  /** 161B20 */
   black87: string;
+  /** 0C171B */
   black89: string;
 }
 
@@ -37,6 +108,7 @@ export interface ThemeType {
   font: FontTypes;
   spacings: SpacingTypes;
   colors: ColorsType;
+  borderRadius: BordersRadius;
 }
 
 export interface ThemeProvider {
