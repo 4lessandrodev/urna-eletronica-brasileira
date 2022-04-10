1c2fc78c7af076522e46a86e4352fec55bcfb674
4lessandrodev <alessandroadm@live.com>
2022-04-10 14:02:01 -0300

    chore(storybook): set loader to typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,20 @@
 const path = require("path");
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  "framework": "@storybook/react",
-  "stories": [
+  framework: "@storybook/react",
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/stories.@(js|jsx|ts|tsx)"
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  staticDirs: ['../public'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
   babel: async (options) => ({
     ...options,
     plugins: [
@@ -16,17 +22,49 @@ module.exports = {
       ["@babel/plugin-transform-react-jsx", { "runtime": "automatic" }]
     ]
   }),
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
     config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
 
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@components/*": path.resolve(__dirname, "../src/components"),
-      "@pages/*": path.resolve(__dirname, "../src/pages"),
-      "@styles/*": path.resolve(__dirname, "../src/styles"),
-      "@types": path.resolve(__dirname, "../src/types/index"),
-    };
+    config.resolve.extensions.push(".ts", ".tsx");
+    
+    config.plugins.push(new webpack.DefinePlugin({
+      'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        domains: [],
+        path: '/',
+        loader: 'default',
+      })
+    }));
 
+    config.plugins.push(new TsconfigPathsPlugin({
+      configFile: path.resolve(__dirname, '..', './tsconfig.json'),
+      baseUrl: path.resolve(__dirname, '..'),
+    }));
+
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|jpg)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true, // webpack@1.x
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
+    });
+
+    // config.resolve.alias = {
+    //   "@components/*": path.resolve(__dirname, "..", "src", "components", "*"),
+    //   "@pages/*": path.resolve(__dirname, "..", "src", "components","*"),
+    //   "@components/*": path.resolve(__dirname, "..", "src", "pages", "*"),
+    //   "@styles/*": path.resolve(__dirname,"..", "src", "styles", "*"),
+    //   "@types": path.resolve(__dirname,"..", "src", "types", "index"),
+    //   ...config.resolve.alias,
+    // };
+    
     return config;
   },
 }
